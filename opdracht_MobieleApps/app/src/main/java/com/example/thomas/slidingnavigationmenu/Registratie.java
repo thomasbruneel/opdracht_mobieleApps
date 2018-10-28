a3008a9a763672b9979209b0100b448f074ca342
thomas.bruneel <thomas.bruneel@hotmail.com>
2018-10-28 20:26:03 +0100

    registreren met gmail lukt via firebase

--- opdracht_MobieleApps/app/src/main/java/com/example/thomas/slidingnavigationmenu/Registratie.java
+++ opdracht_MobieleApps/app/src/main/java/com/example/thomas/slidingnavigationmenu/Registratie.java
@@ -2,13 +2,22 @@ package com.example.thomas.slidingnavigationmenu;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registratie extends AppCompatActivity {
+    private FirebaseAuth fbauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,8 @@ public class Registratie extends AppCompatActivity {
         android.support.v7.app.ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Registratie");
+
+        fbauth=FirebaseAuth.getInstance();
 
 
 
@@ -38,6 +49,21 @@ public class Registratie extends AppCompatActivity {
         EditText etHerhaalWachtwoord=(EditText)findViewById(R.id.uiHerhaalWachtwoord);
         String herhaalWachtwoord=etHerhaalWachtwoord.getText().toString();
         System.out.println(herhaalWachtwoord);
+
+    fbauth.createUserWithEmailAndPassword(gebruikersNaam,wachtwoord)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(Registratie.this,"met succes geregistreerd",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(Registratie.this,"probleempje",Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
 
     }
     public void naarLogin(View view){
