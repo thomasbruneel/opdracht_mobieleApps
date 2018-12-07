1c50230d5fc3c9fbd14c7638fd4331da9ee095b5
thomas.bruneel <thomas.bruneel@hotmail.com>
2018-12-07 17:12:41 +0100

    update

--- opdracht_MobieleApps/app/src/main/java/com/example/thomas/slidingnavigationmenu/MapsActivity.java
+++ opdracht_MobieleApps/app/src/main/java/com/example/thomas/slidingnavigationmenu/MapsActivity.java
@@ -1,25 +1,45 @@
 package com.example.thomas.slidingnavigationmenu;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.example.thomas.slidingnavigationmenu.R;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsActivity extends AppCompatActivity  implements OnMapReadyCallback {
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     String adres;
@@ -28,9 +48,11 @@ public class MapsActivity extends AppCompatActivity  implements OnMapReadyCallba
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         adres = getIntent().getStringExtra("adres");
-        android.support.v7.app.ActionBar actionBar=getSupportActionBar();
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -39,6 +61,18 @@ public class MapsActivity extends AppCompatActivity  implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        try {
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e("google maps", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("google maps", "Can't find style. Error: ", e);
+        }
+
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
         List<Address> addresses = null;
         try {
@@ -51,11 +85,40 @@ public class MapsActivity extends AppCompatActivity  implements OnMapReadyCallba
         double latitude = address.getLatitude();
         LatLng coordinate = new LatLng(latitude, longitude);
         mMap.addMarker(new MarkerOptions().position(coordinate).title(adres));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinate));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(coordinate)
+                .zoom(10).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
+
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
+
+
+
+
 
     public boolean onOptionsItemSelected(MenuItem item){
         finish();
         return true;
     }
+
+
 }
--- opdracht_MobieleApps/app/src/main/java/com/example/thomas/slidingnavigationmenu/TabFragmentProfiel.java
+++ opdracht_MobieleApps/app/src/main/java/com/example/thomas/slidingnavigationmenu/TabFragmentProfiel.java
@@ -1,31 +1,144 @@
 package com.example.thomas.slidingnavigationmenu;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.thomas.slidingnavigationmenu.Models.BiedingDB;
+import com.example.thomas.slidingnavigationmenu.Models.UserDB;
 import com.example.thomas.slidingnavigationmenu.Models.ZoekertjeDB;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TabFragmentProfiel extends Fragment {
     ZoekertjeDB z;
     private BiedingListAdapter adapter;
     ListView mijnListView;
     List<BiedingDB> biedingen=new ArrayList<>();
+    Gson gson;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view=inflater.inflate(R.layout.fragment_tab_fragment_profiel, container, false);
-        TextView tekst=(TextView) view.findViewById(R.id.tekst);
-        tekst.setText("hier moet info komen over de eigenaar van zoekertje....");
 
+        final TextView email =(TextView) view.findViewById(R.id.profielemail);
+        final TextView adres =(TextView) view.findViewById(R.id.profieladres);
+
+        Intent i = getActivity().getIntent();
+        z = (ZoekertjeDB) i.getSerializableExtra("mijnZoekertje");
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        final String idemail = preferences.getString("userID", "");
+
+        gson = new Gson();
+
+        Map<String, String> gegevens = new HashMap<>();
+        gegevens.put("userid", idemail);
+        final JSONObject jsonObject = new JSONObject(gegevens);
+        JSONArray jArray = new JSONArray();
+        jArray.put(jsonObject);
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST,
+                getString(R.string.url) + "/getUserByIdEmail",
+                jArray,
+                new com.android.volley.Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("getUserByIdEmail", response.toString());
+                        Type type = new TypeToken<UserDB>(){}.getType();
+                        UserDB user = gson.fromJson(response.toString().substring(1,response.toString().length()-1), type);
+                        email.setText(user.getEmail());
+                        adres.setText(user.getGemeente());
+
+                    }
+                },
+
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("getUserByIdEmail", "Error: " + error.toString() + ", " + error.getMessage());
+                    }
+                }
+        );
+        VolleyClass.getInstance(getActivity().getApplicationContext()).addToRequestQueue(request, "getUserByIdEmail");
+        Button contact=(Button) view.findViewById(R.id.contact);
+        Button location=(Button) view.findViewById(R.id.location);
+        contact.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(getActivity(),Email.class);
+                intent.putExtra("subject", z.getBeschrijving());
+                startActivity(intent);
+
+            }
+        });
+
+        location.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                final String idemail = preferences.getString("userID", "");
+
+                gson = new Gson();
+                Map<String, String> gegevens = new HashMap<>();
+                gegevens.put("idemail", idemail);
+                final JSONObject jsonObject = new JSONObject(gegevens);
+                JSONArray jArray = new JSONArray();
+                jArray.put(jsonObject);
+                final Intent intent2=new Intent(getActivity(),MapsActivity.class);
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST,
+                        getString(R.string.url) + "/getGemeente",
+                        jArray,
+                        new com.android.volley.Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                Log.d("getGemeente", response.toString());
+                                String myJSONString=response.toString().substring(1,response.toString().length()-1);// [] wegdoen
+                                JsonObject jobj = new Gson().fromJson(myJSONString, JsonObject.class);
+                                String adres = jobj.get("gemeente").toString().substring(1,jobj.get("gemeente").toString().length()-1);
+                                intent2.putExtra("adres",adres);
+                                startActivity(intent2);
+                            }
+                        },
+
+                        new com.android.volley.Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("getGemeente", "Error: " + error.toString() + ", " + error.getMessage());
+                            }
+                        }
+                );
+                VolleyClass.getInstance(getActivity().getApplicationContext()).addToRequestQueue(request, "getGemeente");
+
+
+            }
+        });
         return view;
     }
+
 }
