c99d67a12e07b8e06f577ccf3c46b2c868ba5381
thomas.bruneel <thomas.bruneel@hotmail.com>
2018-12-07 17:37:07 +0100

    update profile tab

--- opdracht_MobieleApps/app/src/main/java/com/example/thomas/slidingnavigationmenu/TabFragmentProfiel.java
+++ opdracht_MobieleApps/app/src/main/java/com/example/thomas/slidingnavigationmenu/TabFragmentProfiel.java
@@ -70,6 +70,11 @@ public class TabFragmentProfiel extends Fragment {
                         UserDB user = gson.fromJson(response.toString().substring(1,response.toString().length()-1), type);
                         email.setText(user.getEmail());
                         adres.setText(user.getGemeente());
+                        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("receiver",user.getEmail());
+                        editor.putString("adres",user.getGemeente());
+                        editor.apply();
 
                     }
                 },
@@ -89,8 +94,12 @@ public class TabFragmentProfiel extends Fragment {
             @Override
             public void onClick(View v)
             {
+
                 Intent intent=new Intent(getActivity(),Email.class);
                 intent.putExtra("subject", z.getBeschrijving());
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                final String receiver = preferences.getString("receiver", "");
+                intent.putExtra("receiver", receiver);
                 startActivity(intent);
 
             }
@@ -102,38 +111,10 @@ public class TabFragmentProfiel extends Fragment {
             public void onClick(View v)
             {
                 SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-                final String idemail = preferences.getString("userID", "");
-
-                gson = new Gson();
-                Map<String, String> gegevens = new HashMap<>();
-                gegevens.put("idemail", idemail);
-                final JSONObject jsonObject = new JSONObject(gegevens);
-                JSONArray jArray = new JSONArray();
-                jArray.put(jsonObject);
+                final String adres = preferences.getString("adres", "");
                 final Intent intent2=new Intent(getActivity(),MapsActivity.class);
-                JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST,
-                        getString(R.string.url) + "/getGemeente",
-                        jArray,
-                        new com.android.volley.Response.Listener<JSONArray>() {
-                            @Override
-                            public void onResponse(JSONArray response) {
-                                Log.d("getGemeente", response.toString());
-                                String myJSONString=response.toString().substring(1,response.toString().length()-1);// [] wegdoen
-                                JsonObject jobj = new Gson().fromJson(myJSONString, JsonObject.class);
-                                String adres = jobj.get("gemeente").toString().substring(1,jobj.get("gemeente").toString().length()-1);
-                                intent2.putExtra("adres",adres);
-                                startActivity(intent2);
-                            }
-                        },
-
-                        new com.android.volley.Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Log.d("getGemeente", "Error: " + error.toString() + ", " + error.getMessage());
-                            }
-                        }
-                );
-                VolleyClass.getInstance(getActivity().getApplicationContext()).addToRequestQueue(request, "getGemeente");
+                intent2.putExtra("adres",adres);
+                startActivity(intent2);
 
 
             }
